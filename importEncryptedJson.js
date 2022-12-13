5c42e152e53dc4861bde484c051643b35244c58f
Nathan-Beckstead-Mtec <Nathan.beckstead6282@stu.Mtec.edu>
2022-12-13 10:13:12 -0700

    working on import

--- importEncryptedJson.js
+++ importEncryptedJson.js
@@ -1,16 +1,21 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const password = require("./password.txt");
+// const password = require("./password.txt");
 
+function readFile(path, callback = () => {}){
+    const fileName = path;
+    const algorithm = 'aes-192-cbc';
+    const password = 'dave';
 
-const fileName = 'cipher.aes';
-const algorithm = 'aes-192-cbc';
-// const password = 'password';
+    const key = crypto.scryptSync(password, 'salt', 24);
+    const iv = Buffer.alloc(16, 0);
+    const inFile = fs.createReadStream(fileName);
+    const decrypt = crypto.createDecipheriv(algorithm, key, iv);
 
-const key = crypto.scryptSync(password, 'salt', 24);
-const iv = Buffer.alloc(16, 0);
-const inFile = fs.createReadStream(fileName);
-const decrypt = crypto.createDecipheriv(algorithm, key, iv);
+    console.log(inFile.pipe(decrypt).toString());
 
-inFile.pipe(decrypt).pipe(outFile);
+}
+
+readFile(process.argv[2]);
+
