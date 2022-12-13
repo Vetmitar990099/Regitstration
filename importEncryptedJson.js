b0bbba14d025df8b947a01a299e692bea1ef0299
Gabriel Amaral <gabrielamaral@Gabriels-MacBook-Air.local>
2022-12-13 10:09:53 -0700

    decrypted file

--- importEncryptedJson.js
+++ importEncryptedJson.js
@@ -1,16 +1,17 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const password = require("./password.txt");
+const password = 'dave';
 
 
-const fileName = 'cipher.aes';
+const fileName = './Encrypted/person.aes';
 const algorithm = 'aes-192-cbc';
 // const password = 'password';
 
 const key = crypto.scryptSync(password, 'salt', 24);
 const iv = Buffer.alloc(16, 0);
 const inFile = fs.createReadStream(fileName);
+const outFile = fs.createWriteStream(fileName + ".json");
 const decrypt = crypto.createDecipheriv(algorithm, key, iv);
 
 inFile.pipe(decrypt).pipe(outFile);
